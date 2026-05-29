6a71f61eefd4890d483aa1bd65e240bb5538975e
KKGS-32 <kkgs32.lab@gmail.com>
2026-05-29 01:08:12 +0000

    update gemini model

--- constant/model.ts
+++ constant/model.ts
@@ -1,14 +1,16 @@
 export const Model: Record<string, string> = {
-  'gemini-flash-latest': 'Gemini Flash Latest',
+  'gemini-3.5-flash': 'Gemini 3.5 Flash',
   'gemini-3-flash-preview': 'Gemini 3 Flash Preview',
   'gemini-3.1-pro-preview': 'Gemini 3.1 Pro Preview',
-  'gemini-3.1-flash-lite-preview': 'Gemini 3.1 Flash-Lite Preview',
+  'gemini-3.1-flash-lite': 'Gemini 3.1 Flash-Lite',
+  'gemini-3.1-flash-image': 'Nano Banana 2',
   'gemini-2.5-flash': 'Gemini 2.5 Flash',
   'gemini-2.5-pro': 'Gemini 2.5 Pro',
+  'gemini-2.5-flash-lite': 'Gemini 2.5 Flash-Lite'
 }
 
-export const OldVisionModel = ['gemini-pro-vision', 'gemini-1.0-pro-vision-latest']
+export const OldVisionModel = []
 
-export const OldTextModel = ['gemini-1.0-pro', 'gemini-1.0-pro-latest', 'gemini-pro']
+export const OldTextModel = ['gemini-2.5-flash', 'gemini-2.5-pro', 'gemini-2.5-flash-lite']
 
-export const DefaultModel = 'gemini-flash-latest'
+export const DefaultModel = 'gemini-3.5-flash'
